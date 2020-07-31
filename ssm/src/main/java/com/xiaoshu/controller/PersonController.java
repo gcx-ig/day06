de8eaca1d8e8e5122c66681a7ec7555d658a490a
guochengxu <guochengxu@qq.com>
2020-07-31 08:08:58 +0800

    作业

---
+++ ssm/src/main/java/com/xiaoshu/controller/PersonController.java
@@ -0,0 +1,194 @@
+package com.xiaoshu.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.xiaoshu.config.util.ConfigUtil;
+import com.xiaoshu.entity.Company;
+import com.xiaoshu.entity.Operation;
+import com.xiaoshu.entity.Person;
+import com.xiaoshu.entity.PersonVo;
+import com.xiaoshu.entity.Role;
+import com.xiaoshu.entity.User;
+import com.xiaoshu.service.OperationService;
+import com.xiaoshu.service.PersonService;
+import com.xiaoshu.service.RoleService;
+import com.xiaoshu.service.UserService;
+import com.xiaoshu.util.StringUtil;
+import com.xiaoshu.util.WriterUtil;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
+
+@Controller
+@RequestMapping("person")
+public class PersonController extends LogController{
+	static Logger logger = Logger.getLogger(PersonController.class);
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private RoleService roleService ;
+	
+	@Autowired
+	private OperationService operationService;
+	
+	@Autowired
+	private PersonService personService;
+	
+	@RequestMapping("personIndex")
+	public String index(HttpServletRequest request,Integer menuid) throws Exception{
+		List<Role> roleList = roleService.findRole(new Role());
+		List<Operation> operationList = operationService.findOperationIdsByMenuid(menuid);
+
+		request.setAttribute("operationList", operationList);
+		request.setAttribute("roleList", roleList);
+		List<Company> clist = personService.findC();
+		request.setAttribute("clist", clist);
+		
+		return "person";
+	}
+	
+	
+	@RequestMapping(value="personList",method=RequestMethod.POST)
+	public void personList(PersonVo personVo,HttpServletRequest request,HttpServletResponse response,String offset,String limit) throws Exception{
+		try {
+			
+			String order = request.getParameter("order");
+			String ordername = request.getParameter("ordername");
+			
+			
+			Integer pageSize = StringUtil.isEmpty(limit)?ConfigUtil.getPageSize():Integer.parseInt(limit);
+			Integer pageNum =  (Integer.parseInt(offset)/pageSize)+1;
+			
+			//PageInfo<User> userList= userService.findUserPage(user,pageNum,pageSize,ordername,order);
+			PageInfo<PersonVo> page = personService.findPage(personVo, pageNum, pageSize);
+			
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("total",page.getTotal() );
+			jsonObj.put("rows", page.getList());
+	        WriterUtil.write(response,jsonObj.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("人员展示错误",e);
+			throw e;
+		}
+	}
+	
+	
+	// 新增或修改
+	@RequestMapping("reserveUser")
+	public void reserveUser(Person person,HttpServletRequest request,User user,HttpServletResponse response){
+		Integer id = person.getId();
+		JSONObject result=new JSONObject();
+		person.setCreateTime(new Date());
+		try {
+			Person name = personService.findByName(person.getExpressName());
+			if (id != null) {   // userId不为空 说明是修改
+				//User userName = userService.existUserWithUserName(user.getUsername());
+				if(name==null|| name != null && name.getId().equals(id)){
+					//user.setUserid(userId);
+					//userService.updateUser(user);
+					
+					personService.updateP(person);
+					result.put("success", true);
+				}else{
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+				
+			}else {   // 添加
+				if(name==null){  // 没有重复可以添加
+					//userService.addUser(user);
+					personService.addP(person);
+					result.put("success", true);
+				} else {
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("保存用户信息错误",e);
+			result.put("success", true);
+			result.put("errorMsg", "对不起，操作失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	// 导入
+		@RequestMapping("importPer")
+		public void importPer(MultipartFile newFile, Person person,HttpServletRequest request,User user,HttpServletResponse response){
+			Integer id = person.getId();
+			JSONObject result=new JSONObject();
+			person.setCreateTime(new Date());
+			try {
+				personService.importPer(newFile);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("导入用户信息错误",e);
+				result.put("success", true);
+				result.put("errorMsg", "对不起，导入失败");
+			}
+			WriterUtil.write(response, result.toString());
+		}
+	
+	@RequestMapping("deleteUser")
+	public void delUser(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			String[] ids=request.getParameter("ids").split(",");
+			for (String id : ids) {
+				//userService.deleteUser(Integer.parseInt(id));
+				personService.delP(Integer.parseInt(id));
+			}
+			result.put("success", true);
+			result.put("delNums", ids.length);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	@RequestMapping("editPassword")
+	public void editPassword(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		String oldpassword = request.getParameter("oldpassword");
+		String newpassword = request.getParameter("newpassword");
+		HttpSession session = request.getSession();
+		User currentUser = (User) session.getAttribute("currentUser");
+		if(currentUser.getPassword().equals(oldpassword)){
+			User user = new User();
+			user.setUserid(currentUser.getUserid());
+			user.setPassword(newpassword);
+			try {
+				userService.updateUser(user);
+				currentUser.setPassword(newpassword);
+				session.removeAttribute("currentUser"); 
+				session.setAttribute("currentUser", currentUser);
+				result.put("success", true);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("修改密码错误",e);
+				result.put("errorMsg", "对不起，修改密码失败");
+			}
+		}else{
+			logger.error(currentUser.getUsername()+"修改密码时原密码输入错误！");
+			result.put("errorMsg", "对不起，原密码输入错误！");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+}
